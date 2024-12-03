2ca586db7248329338d24b6f551681f0b507ccc5
Shravantada <168719302+Shravantada@users.noreply.github.com>
2024-12-03 15:39:12 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull shravantada/simple-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5002:5000 shravantada/simple-python-app
+docker run -d -p 5000:5000 shravantada/simple-python-app
